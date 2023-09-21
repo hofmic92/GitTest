7902adde645ec2b61bf4f14c9468d5a1ab2f608c
Hofman1-J-70b6 <Hofman1-J-70b6@TK6-013-mti.skynet.tul.cz>
2023-09-21 12:03:31 +0200

    Vymakana verze programu

---
+++ src/gitofsonv2/GitofsonV2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitofsonv2;
+
+/**
+ *
+ * @author Hofman1-J-70b6
+ */
+public class GitofsonV2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
