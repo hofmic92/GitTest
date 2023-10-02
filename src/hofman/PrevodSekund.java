c1f1ae92aba4817f5ed0ee161111d21e2af8a744
hofmi <jan1012@seznam.cz>
2023-10-02 19:28:15 +0200

    VI VON ZULUL

---
+++ src/hofman/PrevodSekund.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hofman;
+
+/**
+ *
+ * @author hofmi
+ */
+public class PrevodSekund {
+    public static void main(String[] args) {
+        System.out.println("Zadejter pocet sekund.");
+        System.out.println("bla bla bla ty kundio");
+    }
+    
+}
